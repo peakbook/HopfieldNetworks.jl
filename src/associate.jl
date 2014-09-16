da1cc7fe7e87a22e3b259ea0b9dafeced1f07950
PeakbooK <peakbook@users.noreply.github.com>
2014-09-16 16:14:39 +0900

    change update method

--- src/associate.jl
+++ src/associate.jl
@@ -2,29 +2,52 @@
 export associate!
 
 function update_async!(net::HopfieldNetwork, activatefunc::Function)
-	ord = shuffle!([1:length(net.state)])
-	for i in ord
+	# ord = shuffle!([1:length(net.state)])
+	for i=1:length(net.state) # for i in ord
 		net.state[i] = activatefunc(dot(net.weight[:, i], net.state))
 	end
 	return
 end
 
-function associate!{T <: Number}(net::HopfieldNetwork{T}, pattern::AbstractVector{T}, activatefunc::Function, iterations::Integer = 1_000)
-	copy!(net.state, pattern)
-	associate!(net,activatefunc,iterations)
+function update_sync!(net::HopfieldNetwork, activatefunc::Function)
+ 	state = copy(net.state)
+	for i=1:length(state)
+		net.state[i] = activatefunc(dot(net.weight[:, i], state))
+	end
+	return
 end
 
-function associate!(net::HopfieldNetwork, activatefunc::Function, iterations::Integer = 1_000)
+function associate!{T <: Number}(net::HopfieldNetwork{T}, pattern::AbstractVector{T}, activatefunc::Function, iterations::Integer = 1_000, sync::Bool = false)
+	copy!(net.state, pattern)
+	associate!(net,activatefunc,iterations,sync)
+end
+
+function associate!(net::HopfieldNetwork, activatefunc::Function, iterations::Integer = 1_000, sync::Bool = false)
 	ite = iterations
 	state = copy(net.state)
-	for i in 1:iterations
-		update_async!(net,activatefunc)
-		if state == net.state
-			ite = i
-			break
+
+	if sync
+		state2 = copy(net.state)
+		for i in 1:iterations
+			update_sync!(net,activatefunc)
+			if state2 == net.state
+				ite = i
+				break
+			end
+			copy!(state2,state)
+			copy!(state,net.state)
 		end
-		copy!(state,net.state)
+	else
+		for i in 1:iterations
+			update_async!(net,activatefunc)
+			if state == net.state
+				ite = i
+				break
+			end
+			copy!(state,net.state)
+		end
 	end
+
 	return ite
 end
 
